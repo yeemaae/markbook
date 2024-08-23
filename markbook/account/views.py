a0b9ad8ff0f9fa0d925f1e86b3ab4a18d0e5c744
yeemaae <noobproger@gmail.com>
2024-08-24 00:26:30 +0500

    Added authorization

--- markbook/account/views.py
+++ markbook/account/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 from .forms import LoginForm
+
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
 
 
 def user_login(request):
